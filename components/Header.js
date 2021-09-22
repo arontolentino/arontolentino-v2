8b11979e50625b8ed5570e7ae46b79d0d6aeb474
Aron Tolentino <arontolentino@outlook.com>
2021-09-21 21:09:15 -0400

    Fix build issues

--- components/Header.js
+++ components/Header.js
@@ -154,11 +154,11 @@ export default function Header() {
             <div className="pt-5 pb-6 px-5">
               <div className="flex items-center justify-between">
                 <div>
-                  <img
+                  {/* <img
                     className="h-8 w-auto"
                     src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
                     alt="Workflow"
-                  />
+                  /> */}
                 </div>
                 <div className="-mr-2">
                   <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
