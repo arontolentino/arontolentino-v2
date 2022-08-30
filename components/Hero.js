98f04750fde7feffae2fef2eecd5e727410a3ba3
Aron Tolentino <arontolentino@outlook.com>
2022-08-29 20:20:29 -0400

    Update hero copy

--- components/Hero.js
+++ components/Hero.js
@@ -1,32 +1,14 @@
-/*
-  This example requires Tailwind CSS v2.0+ 
-  
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
-import { ChevronDownIcon } from '@heroicons/react/solid';
-
 export default function Hero() {
   return (
     <div className="bg-white">
       <div className="max-w-7xl mx-auto py-16 sm:py-24 lg:justify-between">
         <h2 className="text-4xl font-extrabold text-gray-900 sm:text-4xl sm:tracking-tight lg:text-5xl">
-          I build full-stack JavaScript applications at Outgive.
+          I build full-stack JavaScript applications.
         </h2>
         <p className="mt-5 text-2xl text-gray-500">
-          During the day, I build analytics and accounting tools for Amazon
-          sellers. At night, I build profitable internet businesses and share my
-          love for software development to anyone who will listen.
+          During the day, I build digital banking products for Canadian
+          Businesses at Vault. At night, I build profitable internet businesses
+          and share my love for software development to anyone who will listen.
         </p>
       </div>
     </div>
